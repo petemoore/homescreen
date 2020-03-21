cf09d8c50c4ca53bf94b27209bd53fcd7a7fc653
Pete Moore <pmoore@mozilla.com>
2020-03-21 14:26:53 +0100

    wip

---
+++ fontimage/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("../font.s")
+	if err != nil {
+		log.Fatalf("Whoops: %v", err)
+	}
+	lines := strings.Split(strings.Replace(string(data), "\r\n", "\n", -1), "\n")
+	for _, a := range lines {
+		fmt.Println(a)
+	}
+}
+
+// 	pngFile, err := os.Open("Lucon_aa_txt_16x16.png")
+// 	if err != nil {
+// 		log.Fatalf("Whoops: %v", err)
+// 	}
+// 	defer func(f *os.File) {
+// 		err := f.Close()
+// 		if err != nil {
+// 			log.Fatalf("Whoops: %v", err)
+// 		}
+// 	}(pngFile)
+// 	img, err := png.Decode(pngFile)
+// 	if err != nil {
+// 		log.Fatalf("Whoops: %v", err)
+// 	}
+//
+// 	fmt.Println(`.global chars
+//
+// .text
+//
+// #################################################################################
+// #################################################################################
+// #### Don't put in a .data section since that will bloat the executable since ####
+// #### .data section will be aligned to a page boundary                        ####
+// #################################################################################
+// #################################################################################
+// # .data  <<------ NO!!!
+//
+//
+// # -------------------------------
+// # THE 'ZX SPECTRUM CHARACTER SET'
+// # -------------------------------
+//
+// .align 5
+// R1_3D00:
+// chars:`)
+//
+// 	for c := 32; c < 128; c++ {
+// 		x := c % 16
+// 		y := c / 16
+// 		fmt.Println("")
+// 		char := string(rune(c))
+// 		if c == 127 {
+// 			char = "(c)"
+// 		}
+// 		fmt.Printf("# 0x%02X - Character: '%v' %v CHR$(%v)\n\n", c, char, strings.Repeat(" ", 9-len(char)), c)
+// 		for j := y * 16; j < (y+1)*16; j++ {
+// 			fmt.Print(`        .hword    0b`)
+//
+// 			for i := x * 16; i < (x+1)*16; i++ {
+// 				r, g, b, a := img.At(i, j).RGBA()
+// 				if r+g+b+a < 20000 {
+// 					fmt.Print("0")
+// 				} else {
+// 					fmt.Print("1")
+// 				}
+// 			}
+// 			fmt.Println("")
+// 		}
+// 		//		fmt.Println("")
+// 	}
