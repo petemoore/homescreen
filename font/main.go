6fb76ee9d21415993f7b3447bfcf692122a6d4e6
Pete Moore <pmoore@mozilla.com>
2020-03-23 09:02:44 +0100

    Updated font

--- font/main.go
+++ font/main.go
@@ -8,8 +8,20 @@ import (
 	"strings"
 )
 
+const (
+	borderWidth       = 4
+	borderHeight      = 4
+	charWidth         = 16
+	charHeight        = 16
+	columns           = 12
+	rows              = 8
+	chars             = 96
+	leadingBlankChars = 0
+	cutoff            = 100000
+)
+
 func main() {
-	pngFile, err := os.Open("Lucon_aa_txt_16x16.png")
+	pngFile, err := os.Open("petefont.png")
 	if err != nil {
 		log.Fatalf("Whoops: %v", err)
 	}
@@ -45,28 +57,28 @@ func main() {
 R1_3D00:
 chars:`)
 
-	for c := 32; c < 128; c++ {
-		x := c % 16
-		y := c / 16
+	for c := 0; c < chars; c++ {
+		x := (c + leadingBlankChars) % columns
+		y := (c + leadingBlankChars) / columns
 		fmt.Println("")
-		char := string(rune(c))
-		if c == 127 {
+		char := string(rune(c + 32))
+		if c == 95 {
 			char = "(c)"
 		}
-		fmt.Printf("# 0x%02X - Character: '%v' %v CHR$(%v)\n\n", c, char, strings.Repeat(" ", 9-len(char)), c)
-		for j := y * 16; j < (y+1)*16; j++ {
+		fmt.Printf("# 0x%02X - Character: '%v' %v CHR$(%v)\n\n", c+32, char, strings.Repeat(" ", 9-len(char)), c+32)
+		for j := y*(charHeight+borderHeight) + borderHeight; j < (y+1)*(charHeight+borderHeight); j++ {
 			fmt.Print(`        .hword    0b`)
 
-			for i := x * 16; i < (x+1)*16; i++ {
+			for i := x*(charWidth+borderWidth) + borderWidth; i < (x+1)*(charWidth+borderWidth); i++ {
 				r, g, b, a := img.At(i, j).RGBA()
-				if r+g+b+a < 20000 {
-					fmt.Print("0")
-				} else {
+				if r+g+b+a < cutoff {
 					fmt.Print("1")
+				} else {
+					fmt.Print("0")
 				}
+				// fmt.Printf("x: %v, y: %v, total: %v\n", i, j, r+g+b+a)
 			}
 			fmt.Println("")
 		}
-		//		fmt.Println("")
 	}
 }
